fb8397f32017d2629a3cc8c9e3298032aa3a6505
Kevin Albrecht <onlyafly@gmail.com>
2018-08-14 22:16:11 +0200

    Compare actual file contents

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -2,11 +2,12 @@
 extern crate quivi;
 
 use std::error::Error;
+use std::ffi::OsStr;
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
-//use std::path::Path;
-use std::ffi::OsStr;
+use std::path::Path;
+use std::path::PathBuf;
 
 #[test]
 fn test_add() {
@@ -21,30 +22,43 @@ fn test_suite() {
         let path = wrapped_path.unwrap().path();
 
         if Some(OsStr::new("q")) == path.extension() {
-            // Create a path to the desired file
-            //let path = Path::new("testsuite/1.q");
-            let display = path.display();
+            let input_contents = read_text_contents(&path);
 
-            println!("file: {}", display);
+            //TODO: calculate input_result correctly
+            let input_result = input_contents.trim_right();
 
-            // Open the path in read-only mode, returns `io::Result<File>`
-            let mut file = match File::open(&path) {
-                // The `description` method of `io::Error` returns a string that
-                // describes the error
-                Err(why) => panic!("couldn't open {}: {}", display, why.description()),
-                Ok(file) => file,
-            };
+            if let Some(output_file_stem) = path.file_stem() {
+                let output_path = Path::new("./testsuite")
+                    .join(output_file_stem.to_str().unwrap().to_owned() + ".out");
+                let output_contents = read_text_contents(&output_path);
 
-            // Read the file contents into a string, returns `io::Result<usize>`
-            let mut s = String::new();
-            match file.read_to_string(&mut s) {
-                Err(why) => panic!("couldn't read {}: {}", display, why.description()),
-                Ok(_) => print!("{} contains:\n{}", display, s),
+                assert_eq!(input_result, output_contents.trim_right());
             }
-
-            // `file` goes out of scope, and the "hello.txt" file gets closed
-
-            assert_eq!("1", s.trim_right());
         }
     }
+}
+
+fn read_text_contents(path: &PathBuf) -> String {
+    // Create a path to the desired file
+    //let path = Path::new("testsuite/1.q");
+    let display = path.display();
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(&path) {
+        // The `description` method of `io::Error` returns a string that
+        // describes the error
+        Err(why) => panic!("couldn't open {}: {}", display, why.description()),
+        Ok(file) => file,
+    };
+
+    // Read the file contents into a string, returns `io::Result<usize>`
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why.description()),
+        Ok(_) => print!("{} contains:\n{}", display, s),
+    }
+
+    // `file` goes out of scope, and the "hello.txt" file gets closed
+
+    return s;
 }
