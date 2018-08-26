51800a58dae31577a51d7d9e13c2ad24cdeabdf0
Kevin Albrecht <onlyafly@gmail.com>
2018-08-26 13:19:51 +0200

    Test case 1 passes

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,31 @@ pub fn add(a: i32, b: i32) -> i32 {
     a + b
 }
 
-pub fn interpret(s: &str) -> &str {
+enum Token {
+    Nil,
+    Generic(String),
+}
+
+pub fn interpret(s: &str) -> String {
+    let tokens = scan(s);
+    let nodes = parse(tokens);
+    let result = eval(nodes);
+
+    result
+}
+
+fn scan(s: &str) -> Vec<Token> {
+    vec![Token::Generic(s.to_string())]
+}
+
+fn parse(tokens: Vec<Token>) -> String {
+    let t = &tokens[0];
+    match t {
+        Token::Generic(s) => s.clone(),
+        Token::Nil => "nil".to_string(),
+    }
+}
+
+fn eval(s: String) -> String {
     s
 }
