5b72214befb6563c4e753b5649f49626a300038a
Kevin Albrecht <onlyafly@gmail.com>
2018-09-08 11:35:39 +0200

    Evaluate the second arg of def special

--- src/eval/specials.rs
+++ src/eval/specials.rs
@@ -2,46 +2,48 @@ use ast::*;
 use eval;
 use eval::env::Env;
 
-pub fn eval_special_list(env: &mut Env, children: Vec<Node>) -> Result<Node, String> {
-    let mut evaled_children = Vec::new();
+pub fn eval_special_list(env: &mut Env, args: Vec<Node>) -> Result<Node, String> {
+    let mut evaled_args = Vec::new();
 
-    for child in children {
+    for child in args {
         let evaled_child = eval::eval_node(env, child)?;
-        evaled_children.push(evaled_child);
+        evaled_args.push(evaled_child);
     }
 
     Ok(Node::List(Box::new(ListObj {
-        children: evaled_children,
+        children: evaled_args,
     })))
 }
 
-pub fn eval_special_quote(mut children: Vec<Node>) -> Result<Node, String> {
-    Ok(children.remove(0))
+pub fn eval_special_quote(mut args: Vec<Node>) -> Result<Node, String> {
+    Ok(args.remove(0))
 }
 
-pub fn eval_special_def(env: &mut Env, mut children: Vec<Node>) -> Result<Node, String> {
-    let name_node = children.remove(0);
+pub fn eval_special_def(env: &mut Env, mut args: Vec<Node>) -> Result<Node, String> {
+    let name_node = args.remove(0);
 
     if let Node::Symbol(name) = name_node {
         if env.exists(&name) {
             return Err(format!("Cannot redefine a name: {}", name));
         }
-        let value = children.remove(0);
-        env.insert(name, value);
+
+        let value_node = super::eval_node(env, args.remove(0))?;
+
+        env.insert(name, value_node);
         Ok(Node::Number(0)) // TODO: should be nil
     } else {
         Err(format!("Expected symbol, got {}", name_node.display()))
     }
 }
 
-pub fn eval_special_update(env: &mut Env, mut children: Vec<Node>) -> Result<Node, String> {
-    let name_node = children.remove(0);
+pub fn eval_special_update(env: &mut Env, mut args: Vec<Node>) -> Result<Node, String> {
+    let name_node = args.remove(0);
 
     if let Node::Symbol(name) = name_node {
         if !env.exists(&name) {
             return Err(format!("Cannot update an undefined name: {}", name));
         }
-        let value = children.remove(0);
+        let value = args.remove(0);
         env.insert(name, value);
         Ok(Node::Number(0)) // TODO: should be nil
     } else {
@@ -82,9 +84,9 @@ pub fn eval_special_update_element(env: &mut Env, mut args: Vec<Node>) -> Result
     }
 }
 
-pub fn eval_special_fn(_env: &mut Env, mut children: Vec<Node>) -> Result<Node, String> {
-    let param_list = children.remove(0);
-    let body = children;
+pub fn eval_special_fn(_env: &mut Env, mut args: Vec<Node>) -> Result<Node, String> {
+    let param_list = args.remove(0);
+    let body = args;
 
     match param_list {
         Node::List(list_node) => Ok(Node::Proc(Box::new(ProcObj {
